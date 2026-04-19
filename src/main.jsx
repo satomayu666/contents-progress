0af6d26386576f0fc9c56f28ae60fc89428a6b66
satomayu666 <satomayu.862421@gmail.com>
2026-04-20 01:14:17 +0900

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AuthProvider from './AuthProvider'
+import AuthProvider from './AuthProvider.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
