dd18104c35713fd2734a01e21fb7106775cbb4de
satomayu666 <satomayu.862421@gmail.com>
2026-04-19 12:19:48 +0900

    fix: exhaustive file name synchronization

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // ここで App.jsx を呼び出しています
-import './index.css'    // もし CSS ファイルがあれば
+import App from './App' 
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
